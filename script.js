ef1d83f4b458845aad4e47a8447db383231139b5
sinuCletus <84879210+sinuCletus@users.noreply.github.com>
2022-10-24 16:20:02 -0700

    Add Event Listener/Create Grid
    I added an event listener for a click event to the input with SUBMIT value.
    This listener will run the userGrid function which displays a text
    that shows the user the size of the grid they have selected.
    
    It also runs the createGrid function. This works and changes the size
    of the grid. However, it does not erase what was previously
    drawn onto the grid. I will have to work on that.

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const grid = document.querySelector('#grid');
+const controls = document.getElementById('controls');
+const cells = document.getElementById('cells');
+const copyCat = document.getElementById('copy-cat');
+const submit = document.getElementById('gridBtn');
 
-let gridSize = 16;
+//create grid size display in a 'number x number' format in the controls div
+submit.addEventListener('click', userGrid);
+function userGrid() {
+        copyCat.innerHTML = cells.value + ' x ' + cells.value;
+        createGrid();
+}
 
 function createGrid() {
-
+const gridSize = cells.value;
     grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
@@ -18,7 +27,3 @@ function createGrid() {
     }
 
 }
-
-
-
-createGrid();
