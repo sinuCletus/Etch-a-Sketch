4338b30017b9e4cf806b609c340e7427ca865d27
sinuCletus <84879210+sinuCletus@users.noreply.github.com>
2022-12-22 11:38:25 -0800

    Slight changes, nothing signiifcant

--- script.js
+++ script.js
@@ -4,11 +4,37 @@ const cells = document.getElementById('cells');
 const copyCat = document.getElementById('copy-cat');
 const submit = document.getElementById('gridBtn');
 
+
+//default grid when page first loads
+defaultGrid();
+
+
 //create grid size display in a 'number x number' format in the controls div
 submit.addEventListener('click', userGrid);
+
+//FUNCTIONS
+
 function userGrid() {
         copyCat.innerHTML = cells.value + ' x ' + cells.value;
         createGrid();
+}
+
+function defaultGrid() {
+    const gridSize = 16;
+
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+    for(let i = 0; i < gridSize * gridSize; i++) {
+        let gridSquare = document.createElement('div');
+
+        gridSquare.classList.add('square');
+        grid.appendChild(gridSquare);
+
+        gridSquare.addEventListener('mouseenter', () => {
+            gridSquare.classList.add('color');
+        });
+    }
 }
 
 function createGrid() {
@@ -18,7 +44,8 @@ function createGrid() {
     grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
     for(let i = 0; i < gridSize * gridSize; i++) {
-        const gridSquare = document.createElement('div');
+        let gridSquare = document.createElement('div');
+
         gridSquare.classList.add('square');
         grid.appendChild(gridSquare);
 
